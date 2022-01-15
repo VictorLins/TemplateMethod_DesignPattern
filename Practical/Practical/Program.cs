f06d65de0694b0be8081b5ccea821acae1abd869
Victor <Victor@DESKTOP-NB7M9F3>
2022-01-15 11:36:38 +0000

    Add project files.

---
+++ Practical/Practical/Program.cs
@@ -0,0 +1,103 @@
+﻿Person lJohn = new John();
+lJohn.DisplayDailyActivityTime();
+
+Person lSimon = new Simon();
+lSimon.DisplayDailyActivityTime();
+
+public abstract class Person
+{
+    public void DisplayDailyActivityTime()
+    {
+        WakeUp();
+
+        EatBreakFast();
+
+        if (HasJob())
+            GoToWork();
+
+        HaveLunch();
+
+        HaveDinner();
+
+        GoToSleep();
+    }
+
+    protected abstract void WakeUp();
+    protected abstract void EatBreakFast();
+    protected abstract bool HasJob();
+    protected virtual void GoToWork() { } // This action will not be mandatory as it depends if the person has job
+    protected abstract void HaveLunch();
+    protected abstract void HaveDinner();
+    protected abstract void GoToSleep();
+}
+
+public class John : Person
+{
+    protected override void EatBreakFast()
+    {
+        Console.WriteLine("John - EatBreakFast - 06:30:00");
+    }
+
+    protected override void GoToSleep()
+    {
+        Console.WriteLine("John - GoToSleep - 22:00:00");
+    }
+
+    protected override void GoToWork()
+    {
+        Console.WriteLine("John - GoToWork - 07:00:00");
+    }
+
+    protected override bool HasJob()
+    {
+        return true;
+    }
+
+    protected override void HaveDinner()
+    {
+        Console.WriteLine("John - HaveDinner - 20:00:00");
+    }
+
+    protected override void HaveLunch()
+    {
+        Console.WriteLine("John - HaveLunch - 12:00:00");
+    }
+
+    protected override void WakeUp()
+    {
+        Console.WriteLine("John - WakeUp - 06:00:00");
+    }
+}
+
+public class Simon : Person
+{
+    protected override void EatBreakFast()
+    {
+        Console.WriteLine("Simon - EatBreakFast - 09:00:00");
+    }
+
+    protected override void GoToSleep()
+    {
+        Console.WriteLine("Simon - GoToSleep - 23:00:00");
+    }
+
+    protected override bool HasJob()
+    {
+        return false;
+    }
+
+    protected override void HaveDinner()
+    {
+        Console.WriteLine("Simon - HaveDinner - 21:00:00");
+    }
+
+    protected override void HaveLunch()
+    {
+        Console.WriteLine("Simon - HaveLunch - 13:00:00");
+    }
+
+    protected override void WakeUp()
+    {
+        Console.WriteLine("Simon - WakeUp - 08:00:00");
+    }
+}
---
+++ Structural/Program.cs
@@ -0,0 +1,42 @@
+﻿AbstractClass lAbstractClassA = new ConcreteClassA();
+lAbstractClassA.TemplateMethod();
+
+AbstractClass lAbstractClassB = new ConcreteClassB();
+lAbstractClassB.TemplateMethod();
+ 
+public abstract class AbstractClass
+{
+    public void TemplateMethod()
+    {
+        Step1();
+        Step2();
+    }
+    protected abstract void Step1();
+    protected abstract void Step2();
+}
+
+public class ConcreteClassA : AbstractClass
+{
+    protected override void Step1()
+    {
+        Console.WriteLine("ConcreteClassA - Step1");
+    }
+
+    protected override void Step2()
+    {
+        Console.WriteLine("ConcreteClassA - Step2");
+    }
+}
+
+public class ConcreteClassB : AbstractClass
+{
+    protected override void Step1()
+    {
+        Console.WriteLine("ConcreteClassB - Step1");
+    }
+
+    protected override void Step2()
+    {
+        Console.WriteLine("ConcreteClassB - Step2");
+    }
+}
